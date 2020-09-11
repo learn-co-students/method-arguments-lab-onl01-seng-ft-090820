0deecd3034ab7d919ce51f4d37673c9fb11783b0
sensible-partial-6157 <daniyellaharmon@gmail.com>
2020-09-11 06:17:42 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -1,9 +1,7 @@
 def introduction(name)
   puts "Hi, my name is #{name}."
 end
-def "#introduction_with_language"
-   puts "and im learning to program in #(language)"
-  end
+
 def introduction_with_language(name,language)
   puts "Hi, My name is #{name} and im learning to program in #{language}"
 end
